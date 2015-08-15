11c0a491cf576e481e4745ab83f00ec76bb62c5a
mingchaoyan <mingchaoyan@gmail.com>
2015-08-15 11:59:41 +0800

    rename

---
+++ exercises-2nd-editioin/16/1/make.erl
@@ -0,0 +1,6 @@
+-module(make).
+-export([need_recopile/1]).
+
+need_recompile(_File) ->
+    true.
+    
---
+++ exercises-2nd-editioin/16/1/make_test.erl
@@ -0,0 +1,8 @@
+-module(make_test).
+
+-include_lib("eunit/include/eunit.hrl")
+start() ->
+    ok.
+
+stop() ->
+    ok.
